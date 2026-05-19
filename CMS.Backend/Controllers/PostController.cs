016abee049fc5a63c468e9da7f030f3f075f6954
hoqbao <baohq2005@gmail.com>
2026-05-19 15:43:23 +0700

    Thêm trang quản lý bài viết và người dùng

---
+++ CMS.Backend/Controllers/PostController.cs
@@ -0,0 +1,33 @@
+﻿using CMS.Data;
+using CMS.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CMS.Backend.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            var posts = new List<Post>
+            {
+                new Post
+                {
+                    Id = 1,
+                    Title = "Lộ trình học ASP.NET Core cho người mới",
+                    Content = "Nội dung bài viết về lộ trình học .NET, MVC, Razor View và Entity Framework Core.",
+                    ImageUrl = "https://via.placeholder.com/300x180?text=ASP.NET+Core"
+                },
+                new Post
+                {
+                    Id = 2,
+                    Title = "ReactJS và WebAPI: Xu hướng Fullstack 2026",
+                    Content = "Nội dung bài viết về kết hợp ReactJS với ASP.NET Core Web API.",
+                    ImageUrl = "https://via.placeholder.com/300x180?text=ReactJS+WebAPI"
+                }
+            };
+
+            return View(posts);
+        }
+    }
+}
---
+++ CMS.Backend/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMS.Data.Entities;
+using System.Collections.Generic;
+
+namespace CMS.Backend.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var users = new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    Username = "admin",
+                    FullName = "Nguyễn Văn Admin",
+                    Role = "Administrator"
+                },
+                new User
+                {
+                    Id = 2,
+                    Username = "editor",
+                    FullName = "Trần Văn Editor",
+                    Role = "Editor"
+                }
+            };
+
+            return View(users);
+        }
+    }
+}
